aaa6738c96b4aba18191f63108d53cb2576ee815
soleil0510 <liangxiaoke0510@126.com>
2019-08-12 17:36:51 +0800

    管理员页面,更新数据库

--- HRStore/models/HR_Database.py
+++ HRStore/models/HR_Database.py
@@ -1,13 +1,15 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class HRUser(models.Model):
     _name = 'hrstore.user'
     _description = 'HRStore User'
+    _rec_name = 'user_id'
 
     user_id = fields.Char('账号', required=True)
     password = fields.Char('密码', required=True)
-    user_type = fields.Char('用户类型')
+    user_type = fields.Selection(string='用户类型', selection=[('0', '管理员'), ('1', '普通用户'), ('2', '供应商')], default='1')
+
 
 
 class HRCommonUser(models.Model):
@@ -36,18 +38,22 @@ class HRProduct(models.Model):
     _description = 'HRStore Product'
 
     pro_name = fields.Char('产品名称')
-    pro_image = fields.Binary(string='图片')
+    pro_image = fields.Binary(string='图片', attachment=True)
     pro_price = fields.Float('价格', (10, 2))
     pro_detail = fields.Text('商品详情')
-    pro_type = fields.Char('产品类型')
-
+    pro_type = fields.Selection(string='产品类型', selection=[('0', '活动'), ('1', '策划'), ('2', '培训产品'), ('3', '精美商品'), ('4', '法律服务')], default='0')
+    state = fields.Selection(string='产品状态', selection=[('0', '待审核'), ('1', '已发布')], default='0')
+    pro_view = fields.Integer(default=0, string='访问量')
     user_id = fields.Many2one(
-        'res.partner',
-        string='商家ID',
+        'hrstore.user',
+        string='供应商',
         ondelete='set null',
-        context={},
-        domain=[],
     )
+
+    @api.multi
+    def update_state(self):
+        self.state = '1'
+
 
 
 class HROrder(models.Model):
@@ -55,15 +61,13 @@ class HROrder(models.Model):
     _description = 'HRStore Order'
 
     order_time = fields.Datetime('下单时间')
-    order_state = fields.Char('订单状态')
+    state = fields.Selection(string='订单状态', selection=[('0', '待处理'), ('1', '已处理')], default='0')
     order_price = fields.Float('价格', (10, 2))
 
     user_id = fields.Many2one(
-        'res.partner',
+        'hrstore.user',
         string='用户ID',
         ondelete='set null',
-        context={},
-        domain=[],
     )
 
     pro_id = fields.Char('产品ID')
@@ -73,7 +77,7 @@ class HRCart(models.Model):
     _name = 'hrstore.cart'
     _description = 'HRStore Cart'
 
-    cart_num = fields.Integer('数量')
+    cart_num = fields.Integer('数量',default=1)
 
     user_id = fields.Char('账号', required=True)
 
@@ -92,19 +96,15 @@ class HRComment(models.Model):
     time = fields.Datetime('评论时间')
 
     user_id = fields.Many2one(
-        'res.partner',
+        'hrstore.user',
         string='用户ID',
         ondelete='set null',
-        context={},
-        domain=[],
     )
 
     pro_id = fields.Many2one(
-        'res.partner',
+        'hrstore.product',
         string='产品ID',
         ondelete='set null',
-        context={},
-        domain=[],
     )
 
 
